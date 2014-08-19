2e05e790e45d4e82d740700c7062a99f3f838409
tise.c <tiseheaini@gmail.com>
2014-08-20 02:14:48 +0800

    使用 sinatra

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://ruby.taobao.org/'
+
+gem 'sinatra'
+gem 'sinatra-contrib'
+
+gem 'sequel'
+gem 'sqlite3'
+
+gem 'puma'
---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+require "sinatra/reloader" if development?
+require 'sinatra/contrib'
+require 'erb'
+require 'sequel'
+
+set :public_folder, File.dirname(__FILE__) + '/static'
+
+get '/' do
+  erb :index
+end
