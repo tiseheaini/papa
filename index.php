afd49b38396c7b69e476dd7d7f6f4a75979af6d4
tise.c <tiseheaini@gmail.com>
2014-08-19 22:27:27 +0800

    添加首页样式

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+  <link rel="stylesheet" href="style/normalize.css" media="all">
+  <link rel="stylesheet" href="style/index.css" media="all">
+</head>
+<body>
+  <div class="main-wrap">
+    <!-- note-text -->
+    <div class="note-text">
+      <img src="images/logo.png" alt="">
+      <form action="article/create.php" class="new-article" id="new-article">
+        <textarea id="article-body" name="aritcle[body]" cols="30" rows="10"></textarea>
+        <input type="submit" value="submit">
+      </form>
+    </div>
+    <!-- end note-text -->
+
+    <!-- acrticles -->
+    <div class="articles">
+      <div class="article">
+        <div class="article-body">播放英语</div>
+        <div class="article-time">12:19</div>
+      </div>
+      <div class="article">
+        <div class="article-body">播放英语</div>
+        <div class="article-time">12:19</div>
+      </div>
+      <div class="article">
+        <div class="article-body">播放英语</div>
+        <div class="article-time">12:19</div>
+      </div>
+      <div class="article">
+        <div class="article-body">播放英语</div>
+        <div class="article-time">12:19</div>
+      </div>
+      <div class="article">
+        <div class="article-body">播放英语</div>
+        <div class="article-time">12:19</div>
+      </div>
+      <div class="article">
+        <div class="article-body">播放英语</div>
+        <div class="article-time">12:19</div>
+      </div>
+    </div>
+    <!-- end acrticles -->
+  </div>
+</body>
+</html>
